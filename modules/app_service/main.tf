d4b9f94f52122562e558fb18c6de9ee22d313c52
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2025-08-24 22:16:49 +0300

    Add Azure service plan and Linux web app resources

---
+++ modules/app_service/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_service_plan" "plan" {
+  name                = "${var.prefix}-plan"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "${var.prefix}-webapp"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
+  }
+  
+  identity {
+    type = "SystemAssigned"
+  }
+}
