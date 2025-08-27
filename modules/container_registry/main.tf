d4d59a3c2832766ca4a315a296f26f26e04f61ba
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2025-08-27 03:44:16 +0300

    Add Azure Container Registry resource configuration

---
+++ modules/container_registry/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.prefix}acr"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true # Enables simple username/password login for the CI/CD pipeline
+}
