71b93a6b68614bf8626b85c3209bcf81d6488edc
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2025-08-27 03:57:12 +0300

    Change container registry name to somerandomacrname

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.prefix}acr"
+  name                = "somerandomacrname"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
