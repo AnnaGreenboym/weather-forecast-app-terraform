c7fc425da28d5c59301a7c8e7868db6a55d59738
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2025-08-27 03:47:33 +0300

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -12,3 +12,24 @@ variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
 }
+
+variable "acr_login_server" {
+  description = "The login server of the Azure Container Registry."
+  type        = string
+}
+
+variable "acr_admin_username" {
+  description = "The admin username for the ACR."
+  type        = string
+}
+
+variable "acr_admin_password" {
+  description = "The admin password for the ACR."
+  type        = string
+  sensitive   = true
+}
+
+variable "docker_image_name" {
+  description = "The name of the Docker image."
+  type        = string
+}
