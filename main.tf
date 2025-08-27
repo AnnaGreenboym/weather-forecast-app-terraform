e8e25fe7df5454a9c849d06dada32cbd203f5a22
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2025-08-27 03:41:25 +0300

    Add Azure Container Registry module and update App Service

--- main.tf
+++ main.tf
@@ -1,28 +1,40 @@
 data "http" "myip" {
   url = "https://api.ipify.org"
 }
-
-# Data source to get information about your Azure account
 data "azurerm_client_config" "current" {}
 
-# 1. Resource Group - This stays in the root module as it's the container for everything.
+# 1. Resource Group (no change)
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
 
-# 2. App Service Module
-module "app_service" {
-  source = "./modules/app_service" # Path to the module folder
+# 2. Azure Container Registry (ACR) Module - NEW
+module "acr" {
+  source = "./modules/container_registry"
 
   prefix              = var.prefix
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# 3. Database Module
+# 3. App Service Module - UPDATED
+# Now takes the ACR details as input.
+module "app_service" {
+  source = "./modules/app_service"
+
+  prefix                  = var.prefix
+  location                = var.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  acr_login_server        = module.acr.login_server
+  acr_admin_username      = module.acr.admin_username
+  acr_admin_password      = module.acr.admin_password
+  docker_image_name       = var.docker_image_name
+}
+
+# 4. Database Module (no change)
 module "database" {
-  source = "./modules/database" # Path to the module folder
+  source = "./modules/database"
 
   prefix                 = var.prefix
   location               = var.location
@@ -32,13 +44,13 @@ module "database" {
   local_developer_ip     = data.http.myip.response_body
 }
 
-# 4. Key Vault Module
+# 5. Key Vault Module (no change)
 module "key_vault" {
-  source = "./modules/key_vault" # Path to the module folder
+  source = "./modules/key_vault"
 
-  prefix                  = var.prefix
-  location                = var.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  app_service_principal_id = module.app_service.app_identity_principal_id # Get output from another module
+  prefix                   = var.prefix
+  location                 = var.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  app_service_principal_id = module.app_service.app_identity_principal_id
 }
