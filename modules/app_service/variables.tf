2f8063f77809df9d87f182fffa21e2319ddcd3ab
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2026-01-21 19:44:10 +0200

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -12,3 +12,18 @@ variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
 }
+
+variable "login_server" {
+  description = "ACR login server"
+  type        = string
+}
+
+variable "image_name" {
+  description = "image name"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "image tag"
+  type        = string
+}
