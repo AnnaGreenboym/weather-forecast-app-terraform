be42ea02c0954f986d6295a14077b9725c3ccfb4
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2026-01-21 23:58:15 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "prefix" {
 variable "location" {
   description = "The Azure region where resources will be created."
   type        = string
-  default     = "West Europe"
+  default     = "North Europe"
 }
 
 variable "postgres_admin_login" {
