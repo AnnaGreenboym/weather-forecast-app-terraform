5447357a600328d6604eaaf1e4a176e1671a790d
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2026-01-23 21:03:55 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-working-israel"
-    storage_account_name = "stweathertf"
+    storage_account_name = "stweathert"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
