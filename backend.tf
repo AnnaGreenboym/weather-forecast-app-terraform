bc8e93bb410c367483d7a1b969690fde3a5a32b6
AnnaGreenboym <100298004+AnnaGreenboym@users.noreply.github.com>
2026-01-23 22:00:06 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-working-israel"
-    storage_account_name = "stweathert"
+    storage_account_name = "weatherstoragetf"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
